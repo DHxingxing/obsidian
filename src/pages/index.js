ce3ef2d6e8047b4361fe925c4fa492b35099339a
DHxingxing <3237779503@qq.com>
2025-07-20 20:44:15 +0800

    修改背景图

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import './index.module.css'
+// 确保导入了 CSS Module
+import styles from './index.module.css';
 
 export default function Home() {
     const {siteConfig} = useDocusaurusContext();
@@ -12,21 +13,25 @@ export default function Home() {
             title={siteConfig.title}
             description={siteConfig.tagline}
         >
-           <div className="homePage=bg"> </div>
+            {/* 1. 这是我们的背景图容器 */}
+            <div className={styles.homePageBg}></div>
 
-            {/* 中心按钮 */}
-            <div style={{textAlign: 'center', marginTop: '2rem'}}>
-                <Link
-                    className="button button--primary button--lg"
-                    to="/docs/intro"
-                >
-                    Let's go
-                </Link>
-            </div>
+            {/* 2. 我们将所有页面内容包裹在一个容器中，方便居中和管理 */}
+            <div className={styles.homePageContent}>
+                {/* 中心按钮 */}
+                <div style={{textAlign: 'center', marginBottom: '2rem'}}>
+                    <Link
+                        className="button button--primary button--lg"
+                        to="/docs/intro"
+                    >
+                        Let's go
+                    </Link>
+                </div>
 
-            {/* 介绍文字 */}
-            <div style={{textAlign:'center', marginTop:'2rem', fontStyle:'italic', fontSize:'1.5rem'}}>
-                “Talk is cheap. Show me the code.”
+                {/* 介绍文字 */}
+                <div className={styles.quote}>
+                    “Talk is cheap. Show me the code.”
+                </div>
             </div>
         </Layout>
     );
